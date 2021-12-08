65992cf8a242e52590a76dcc9054934b51827971
Zack Lyon <zack.j.lyon@gmail.com>
2021-12-07 18:11:16 -0800

    Add home page styling

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div style={{ backgroundImage: 'url(kickball.jpg)' }} className='home-page'>
+      <div className='kickball-title'>PNW Kickball League</div>
+      <div className='acknowledgement'>Photo by cottonbro from Pexels</div>
     </div>
-  )
+  );
 }
